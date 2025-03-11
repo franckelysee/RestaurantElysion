62ca01791b39b717907de8c5f973ab6c305812fd
Franck Elysee <franckelysee671@gmail.com>
2025-03-11 16:10:05 +0100

    menu pagination

--- public/js/utils/menu/menu.js
+++ public/js/utils/menu/menu.js
@@ -1,27 +1,93 @@
 
 
-const pageMenus = document.querySelectorAll('.page-menu');
-console.log(pageMenus);
-if(pageMenus){
-    pageMenus.forEach(menu => {
-        menu.addEventListener('click', () => {
-            if(menu.classList.contains('page-left')){
-                menu.classList.remove('back');
-                menu.classList.add('turn');
-            }else if(menu.classList.contains('back')){
-                menu.classList.remove('back');
-                menu.classList.add('turn');
-            }
-            else if(menu.classList.contains('turn')){
-                menu.classList.remove('turn');
-                menu.classList.add('back');
-            }
-            else{
-                menu.classList.remove('turn');
-                menu.classList.add('back');
-            }
+class MenusBook{
+    constructor(){
+        this.init();
+    }
+
+    init(){
+        this.updateZindex();
+        this.paginatePages();
+    }
+
+    paginatePages(){
+        const pages = document.querySelectorAll('.page-menu');
+        const btn_left = document.querySelector('.btn-turn-left');
+        const btn_right = document.querySelector('.btn-turn-right');
+        var btn_right_span = document.querySelector('.btn-turn-right span');
+        var btn_left_span = document.querySelector('.btn-turn-left span');
+
+        btn_left.addEventListener('click', () => {
+            pages.forEach(page => {
+                if(page.classList.contains('page-left') && page.previousElementSibling){
+                    let pagedata = page.getAttribute('data-page');
+                    let btndata = btn_left.getAttribute('data-page');
+                    if(pagedata == btndata){
+                        this.TurnLeftPage(page);
+                        btn_left.setAttribute('data-page', parseInt(btndata) - 1);
+                        btn_right.setAttribute('data-page', parseInt(btndata));
+                        btn_right_span.innerText = "Tourne la Page " + (parseInt(btndata));
+                        btn_left_span.innerText = "Tourne la Page " + (parseInt(btndata) - 1);
+                    }
+                }
+            });
+            this.updateZindex();
         }
         );
-    });
+
+        btn_right.addEventListener('click', () => {
+            pages.forEach(page => {
+                if(page.classList.contains('page-right') && page.nextElementSibling){
+                    let pagedata = page.getAttribute('data-page');
+                    let btndata = btn_right.getAttribute('data-page');
+                    if(pagedata == btndata){
+                        this.TurnRightPage(page);
+                        btn_right.setAttribute('data-page', parseInt(btndata) + 1);
+                        btn_left.setAttribute('data-page', parseInt(btndata));
+                        btn_right_span.innerText = "Tourne la Page " + (parseInt(btndata) + 1);
+                        btn_left_span.innerText = "Tourne la Page " + (parseInt(btndata));
+                    }
+                }
+            });
+            this.updateZindex();
+        }
+        );
+    }
+
+    TurnRightPage(page){
+        // je vérifie si la page est une page de gauche ou de droite et que ce n'est pas la première page
+        // if(page.previousElementSibling){
+        //     page.classList.add('page')
+        // }
+        page.classList.remove('page-right');
+        page.classList.remove('go-right');
+        page.classList.add('go-left');
+        page.classList.add('page-left');
+
+    }
+    TurnLeftPage(page){
+        page.classList.remove('page-left');
+        page.classList.remove('go-left');
+        page.classList.add('go-right');
+        page.classList.add('page-right');
+    }
+    updateZindex(){
+        const pagesRight = document.querySelectorAll(".page-right");
+        const pagesLeft = document.querySelectorAll(".page-left");
+        let zIndexRight = pagesRight.length;
+        let zIndexLeft = 0;
+
+        pagesRight.forEach((page) => {
+          page.style.zIndex = zIndexRight;
+          zIndexRight--;
+        });
+
+        pagesLeft.forEach((page) => {
+            page.style.zIndex = zIndexLeft;
+            zIndexLeft++;
+        });
+    }
+
 }
 
+new MenusBook();
