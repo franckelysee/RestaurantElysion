eabccce2b7b7a315661eb21247c82b3b1da653bd
Franck Elysee <franckelysee671@gmail.com>
2025-03-17 12:21:37 +0100

    wip

--- public/js/utils/carousel.js
+++ public/js/utils/carousel.js
@@ -1,135 +1,105 @@
+export default class Carousel {
+    loop = false;
+    autoScroll = null; // Initialisé à null pour plus de clarté
 
+    constructor(el, both_mob_desk = false, is_loop = false, auto = true) {
+        this.loop = is_loop;
+        if (window.matchMedia('(min-width: 1024px)').matches || both_mob_desk) {
+            this.nextButton = el.querySelector('[data-slider-next]');
+            this.prevButton = el.querySelector('[data-slider-prev]');
+            this.wrapper = el.querySelector('[data-slider-wrapper]');
 
-export default class Carousel{
+            // Ajout d'un flag pour suivre l'état auto
+            this.isAutoRunning = auto;
 
-    /**
-         * @param {HTMLDivElement} el
-         * @param {boolean} both_mob_desk  true if for both mobile and desktop devices, false otherwise  (default is false)
-         * @param {boolean} is_loop  true if for loop, false otherwise  (default is false)
-         * @param {boolean} auto  true if auto run, false otherwise  (default is true)
-         */
-        loop = false
-        autoScroll;
-        constructor(el, both_mob_desk = false, is_loop = false, auto = true) {
-            if (window.matchMedia('(min-width: 1024px)').matches || both_mob_desk) {
-
-                this.nextButton = el.querySelector('[data-slider-next]');
-                this.prevButton = el.querySelector('[data-slider-prev]');
-                this.wrapper = el.querySelector('[data-slider-wrapper]');
-                this.nextButton.addEventListener('click', () => {
-                    auto = false;
-                    this.autoRun(false)
-                    this.move(1);
-                });
-                this.prevButton.addEventListener('click', () => {
-                    auto = false;
-                    this.autoRun(false)
-                    this.move(-1);
-                });
-                this.wrapper.addEventListener('scrollend', () => {
-                    this.updateCarousel();
-                });
-                this.updateCarousel();
-                if (auto) {
-                    this.autoRun();
-                }
-            }
-        }
-
-        get itemToScroll() {
-            return parseInt(window.getComputedStyle(this.wrapper)
-                .getPropertyValue('--items'), 10)
-        }
-        get pages() {
-            return Math.floor(this.wrapper.children.length / this.itemToScroll) || 1;
-        }
-
-        get currentpage() {
-            let value = this.wrapper.scrollLeft / this.wrapper.offsetWidth;
-            if (value < 0.01 || isNaN(value)) {
-                value = 0
-            }
-
-            return Math.floor(value);
-        }
-
-
-
-
-        updateCarousel() {
-            if (this.currentpage == 0) {
-                if (!this.prevButton.classList.contains('carousel-prev')){
-                    this.prevButton.classList.add('hidden');
-                }
-            } else {
-                this.prevButton.classList.remove('hidden');
-            }
-
-            if (this.currentpage == this.pages - 1 && this.wrapper.scrollLeft + this.wrapper.offsetWidth >= this.wrapper.scrollWidth) {
-                if (!this.nextButton.classList.contains('carousel-next')){
-                    this.nextButton.classList.add('hidden');
-                }
-            } else {
-                this.nextButton.classList.remove('hidden');
-            }
-        }
-
-
-        autoRun(active=true) {
-            console.log('in the auto function current page')
-            if (!active) {
-                clearInterval(this.autoScroll)
-            }else{
-
-                let i = 1;
-                this.autoScroll = setInterval(() => {
-                    console.log('current page ', this.currentpage, " pages ", this.pages)
-                    console.log('------------------------------------------------------- ')
-                    if (this.currentpage < this.pages -1 ) {
-                        this.move(i);
-                    }
-                    // else if (this.currentpage == this.pages -1){
-                    //     console.log('current p ', this.currentpage, 'i ', i)
-                    //     if (i ==1){
-                    //         this.move(i)
-                    //         i = -1
-                    //     }else{
-                    //         this.move(i)
-                    //         i = 1
-                    //     }
-                    // }
-                    else {
-                        clearInterval(this.autoScroll);
-                    }
-                }, 5000);
-                // Pause automatique au survol
-                this.wrapper.addEventListener('mouseenter', () => this.autoRun(false));
-                this.wrapper.addEventListener('mouseleave', () => this.autoRun());
-            }
-
-
-        }
-
-
-        /**
-         *
-         * @param {number} number
-         */
-        move(number) {
-            var newPage = this.currentpage + number;
-            if (newPage < 0) {
-                newPage = this.pages -1 ; //0
-            }
-            if (newPage >= this.pages) {
-                // newPage = 0;
-                this.pages - 1;
-            }
-
-            this.wrapper.scrollTo({
-                left: this.wrapper.children[newPage * this.itemToScroll].offsetLeft,
-                behavior: "smooth"
+            this.nextButton.addEventListener('click', () => {
+                this.stopAutoScroll();
+                this.move(1);
             });
 
+            this.prevButton.addEventListener('click', () => {
+                this.stopAutoScroll();
+                this.move(-1);
+            });
+
+            this.wrapper.addEventListener('scrollend', () => {
+                this.updateCarousel();
+            });
+
+            this.updateCarousel();
+            if (auto) {
+                this.startAutoScroll();
+            }
+
+            // Gestion du survol
+            this.wrapper.addEventListener('mouseenter', () => this.stopAutoScroll());
+            this.wrapper.addEventListener('mouseleave', () => {
+                if (this.isAutoRunning) this.startAutoScroll();
+            });
+        }
+    }
+
+    get itemToScroll() {
+        return parseInt(window.getComputedStyle(this.wrapper)
+            .getPropertyValue('--items'), 10);
+    }
+
+    get pages() {
+        return Math.floor(this.wrapper.children.length / this.itemToScroll) || 1;
+    }
+
+    get currentpage() {
+        let value = this.wrapper.scrollLeft / this.wrapper.offsetWidth;
+        if (value < 0.01 || isNaN(value)) {
+            value = 0;
+        }
+        return Math.floor(value);
+    }
+
+    updateCarousel() {
+        this.prevButton.classList.toggle('hidden', this.currentpage === 0);
+        this.nextButton.classList.toggle('hidden',
+            this.currentpage === this.pages - 1 &&
+            this.wrapper.scrollLeft + this.wrapper.offsetWidth >= this.wrapper.scrollWidth
+        );
+    }
+
+    startAutoScroll() {
+        if (this.autoScroll) return; // Évite les multiples intervalles
+        this.autoScroll = setInterval(() => {
+            if (this.currentpage < this.pages - 1) {
+                this.move(1);
+            } else if (this.loop) {
+                this.move(-(this.pages - 1));
+            } else {
+                this.stopAutoScroll();
+            }
+        }, 5000);
+    }
+
+    stopAutoScroll() {
+        if (this.autoScroll) {
+            clearInterval(this.autoScroll);
+            this.autoScroll = null;
+        }
+    }
+
+    move(number) {
+        let newPage = this.currentpage + number;
+
+        if (this.loop) {
+            if (newPage < 0) {
+                newPage = this.pages - 1;
+            } else if (newPage >= this.pages) {
+                newPage = 0;
+            }
+        } else {
+            newPage = Math.max(0, Math.min(newPage, this.pages - 1));
         }
 
+        this.wrapper.scrollTo({
+            left: this.wrapper.children[newPage * this.itemToScroll].offsetLeft,
+            behavior: "smooth"
+        });
+    }
 }
